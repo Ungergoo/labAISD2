9c47e2ffc56a7429e10594583365c2ef92327aba
Ungergoo <47298342+Ungergoo@users.noreply.github.com>
2025-02-19 18:15:43 +0400

    Update lab 2.py

--- lab 2.py
+++ lab 2.py
@@ -5,24 +5,24 @@
 
 #Натуральные числа, содержащие ровно К цифр. Вывести количество таких чисел. Минимальное число вывести прописью.
 import re
-k = 3; c = 0
-clearm = []; massiv2 = []
-abc = ('ноль', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять')
+
 f = open('labtext.txt', 'r')
-massiv = f.read().split()
-for l in range (len(massiv)):
-    s1 = re.sub("[^0-9]", "", massiv[c])
-    clearm.append(s1)
-    c+=1
-for i in range(len(clearm)):
-    if len(clearm[i]) == k:
-        massiv2.append(clearm[i])
-if massiv2 == []:
+massiv = f.read()
+massiv2 = []
+k = 2
+spisok = '0123456789'
+
+num_dict = {
+    '0': 'ноль', '1': 'один', '2': 'два', '3': 'три', '4': 'четыре',
+    '5': 'пять', '6': 'шесть', '7': 'семь', '8': 'восемь', '9': 'девять'
+}
+
+clearm = [num for num in re.findall(r'(?<![-+])\b[1-9][0-9]*\b(?![.,])', massiv) if len(num) == k]
+
+if not clearm:
     print("Таких элементов нет")
-    exit()
-minnum = (min(massiv2))
-print(len(massiv2))
-for j in str(minnum):
-    for k in range(10):
-        if j == str(k):
-            print(abc[k])
+else:
+    minnum = min(map(int, clearm))
+    print("Количество чисел: ", len(clearm))
+    print("Минимальное число прописью:")
+    print(" ".join(num_dict[j] for j in str(minnum)))
